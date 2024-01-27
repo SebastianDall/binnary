fe6dcca68c1ef7e3ff7c28f671354f85bba91ac2
SebastianDall <semoda@bio.aau.dk>
2024-01-27 17:37:06 +0000

    feat: setup main entrypoint and structure

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # binnairy
 Binary methylation pattern binning
+
+
+## Install
+
+### Conda Environment
+In the devcontainer, run the following commands to create a conda environment with the required dependencies:
+
+```bash
+micromamba create -n nanomotif -c conda-forge python=3.9
+micromamba activate nanomotif
+#python -m pip install nanomotif
+```
---
+++ binnary.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+
+import argparse
+import pandas as pd
+# Import other necessary libraries here
+
+def main(args):
+    # Placeholder for your logic to handle the files and analysis
+    print("hello Binnary")
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="DNA Methylation Pattern Analysis")
+
+    # Adding command-line options
+    parser.add_argument("--motifs_scored", type=str, help="Path to motifs-scored.tsv file")
+    parser.add_argument("--bin_motifs", type=str, help="Path to bin-motifs.tsv file")
+    parser.add_argument("--contig_bins", type=str, help="Path to bins.tsv file for contig bins")
+    parser.add_argument("--assembly_stats", type=str, help="Path to assembly_info.txt file")
+    parser.add_argument("--assembly_file", type=str, help="Path to assembly.fasta file")
+    parser.add_argument("--mean_methylation_cutoff", type=float, default=0.25, help="Cutoff value for considering a motif as methylated")
+    parser.add_argument("--n_motif_cutoff", type=int, default=6, help="Number of motifs observed before considering a motif valid")
+    parser.add_argument("--kmer_window_size", type=int, default=4, help="kmer window size")
+    parser.add_argument("--out", type=str, default="contig-bin-association.tsv", help="Path to output filename")
+
+    args = parser.parse_args()
+    main(args)
