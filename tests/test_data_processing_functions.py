8bbf894c9b1f3940e0f0b0f49d6de60d35946785
SebastianDall <semoda@bio.aau.dk>
2024-01-31 09:35:34 +0000

    added test for motifs_scored_in_bins

--- tests/test_data_processing_functions.py
+++ tests/test_data_processing_functions.py
@@ -36,3 +36,42 @@ def test_feature_with_loaded_data(loaded_data):
     # assembly file
     assert assembly_file is not None
     assert len(assembly_file) == 16
+    
+    
+    
+    
+def test_motifs_scored_in_bins(loaded_data):
+    # Access the loaded data directly if returned as a dictionary
+    motifs_scored = loaded_data["motifs_scored"]
+    bin_motifs = loaded_data["bin_motifs"]
+    contig_bins = loaded_data["contig_bins"]
+    assembly_stats = loaded_data["assembly_stats"]
+    
+    # Alter bin_motifs to include motif_mod and mean
+    bin_motifs["motif_mod"] = bin_motifs["motif"] + "_" + bin_motifs["mod_type"]
+    # Calculate n_motifs and mean methylation
+    bin_motifs["n_motifs"] = bin_motifs["n_mod_bin"] + bin_motifs["n_nomod_bin"]
+    bin_motifs["mean"] = bin_motifs["n_mod_bin"] / bin_motifs["n_motifs"]
+    
+    
+    
+    # Step 2: create motifs_scored_in_bins
+    motifs_scored_in_bins = data_processing.prepare_motifs_scored_in_bins(
+        motifs_scored, bin_motifs, contig_bins, assembly_stats, 
+    )
+    
+    # bin_motifs
+    ## Assert there are only the m1, m2, m3 in motif_mod
+    assert sorted(bin_motifs["motif_mod"].unique()) == sorted(["m1_a", "m2_a", "m3_a", "m6_a"])
+    
+    # motifs_scored_in_bins
+    print(motifs_scored_in_bins)
+    assert sorted(bin_motifs["motif_mod"].unique().tolist()) == sorted(motifs_scored_in_bins["motif_mod"].unique().tolist())
+    assert motifs_scored_in_bins.shape[1] == 13 # number of columns
+    # Assert contig 1 belongs to bin 1
+    assert motifs_scored_in_bins.loc[motifs_scored_in_bins["contig"] == "contig_1", "bin"].unique() == "b1"
+    
+    
+    
+
+    
