20612ec2b46fb2c002f9c6687f2be03d64e574ac
SebastianDall <sebastiandall1@hotmail.com>
2024-01-31 08:37:59 +0100

    feat: added subparser

--- src/cli_parser.py
+++ src/cli_parser.py
@@ -2,10 +2,11 @@ import argparse
 
 def add_common_arguments(subparser):
     """Function to add common arguments to a subparser."""
-    subparser.add_argument("--assembly_stats", type=str, help="Path to assembly_info.txt file")
-    subparser.add_argument("--assembly_file", type=str, help="Path to assembly.fasta file")
+    subparser.add_argument("--motifs_scored", type=str, help="Path to motifs-scored.tsv from nanomotif")
     subparser.add_argument("--bin_motifs", type=str, help="Path to bin-motifs.tsv file")
     subparser.add_argument("--contig_bins", type=str, help="Path to bins.tsv file for contig bins")
+    subparser.add_argument("--assembly_stats", type=str, help="Path to assembly_info.txt file")
+    subparser.add_argument("--assembly_file", type=str, help="Path to assembly.fasta file")
     subparser.add_argument("--mean_methylation_cutoff", type=float, default=0.50, help="Cutoff value for considering a motif as methylated")
     subparser.add_argument("--n_motif_cutoff", type=int, default=6, help="Number of motifs observed before considering a motif valid")
     subparser.add_argument("--out", type=str, help="Path to output filename")
