b2c83be51c89f57d92a8d9126112d8855c136005
SebastianDall <semoda@bio.aau.dk>
2024-02-07 09:21:21 +0100

    feat: added include_contigs file

---
+++ src/include_contigs.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from src import data_processing as dp 
+
+
+
+
+def include_contigs(motifs_scored_in_bins, bin_motif_binary, args):
+    """
+    
+    
+    """
+    
+    
