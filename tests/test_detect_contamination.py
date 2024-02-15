f2bb36d3ab9984ba017bad249c8ae52e0b39a112
SebastianDall <semoda@bio.aau.dk>
2024-02-15 12:38:57 +0000

    fix: fixed test

--- tests/test_detect_contamination.py
+++ tests/test_detect_contamination.py
@@ -21,6 +21,6 @@ def test_detect_contamination(loaded_data, motifs_scored_in_bins_and_bin_motifs)
     assert contaminated_contigs is not None
     assert contaminated_contigs["bin"].unique().tolist() == ["b3"]
     assert sorted(contaminated_contigs["contig"].unique().tolist()) == ["contig_12", "contig_13", "contig_6"]
-    assert contaminated_contigs[contaminated_contigs["contig"] == "contig_6"]["binary_methylation_missmatch_score"].values[0] == 2.0
+    assert contaminated_contigs[contaminated_contigs["contig"] == "contig_6"]["binary_methylation_missmatch_score"].values[0] == 3.0
     assert contaminated_contigs[contaminated_contigs["contig"] == "contig_6"]["alternative_bin"].unique() == ["b2"]
     
