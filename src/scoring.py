8079a197705a77f4b4fc9bad578c76e102418ccc
SebastianDall <semoda@bio.aau.dk>
2024-02-23 13:53:55 +0000

    chore: removed code

--- src/scoring.py
+++ src/scoring.py
@@ -93,51 +93,14 @@ def compare_methylation_pattern(motif_binary_compare, choices):
     motif_binary_compare = motif_binary_compare.with_columns(motif_comparison_score.alias("motif_comparison_score"))
     
     # Group by bin and bin_compare and calculate the sum of the motif_comparison_score and the count of non-NA values
-    # contig_bin_comparison_score = motif_binary_compare \
-    #     .group_by(["bin", "bin_compare"]) \
-    #     .agg(
-    #         pl.sum("motif_comparison_score").alias("binary_methylation_missmatch_score"),
-    #         pl.count("motif_comparison_score").alias("non_na_comparisons")
-    #     ) \
-    #     .select(
-    #         pl.col('bin_compare').str.split('_').arr.to_struct(n_field_strategy="max_width")
-    #     ).unnest('bin_compare') \
-    #     .drop("contig_number")
-        
     contig_bin_comparison_score = motif_binary_compare \
         .group_by(["bin", "bin_compare"]) \
         .agg([
             pl.sum("motif_comparison_score").alias("binary_methylation_missmatch_score"),
             pl.count("motif_comparison_score").alias("non_na_comparisons")
         ])
-    # contig_bin_comparison_score = motif_binary_compare.groupby(["bin", "bin_compare"]).agg(
-    #     binary_methylation_missmatch_score=pd.NamedAgg(column="motif_comparison_score", aggfunc="sum"),
-    #     non_na_comparisons=pd.NamedAgg(column="motif_comparison_score", aggfunc="count")  # Count non-NA values
-    # ).reset_index()
-    
-    # Split bin_compare into bin and contig
-    # contig_bin_comparison_score[["contig_bin", "contig", "contig_number"]] = contig_bin_comparison_score["bin_compare"].str.split("_", expand=True)
-    # contig_bin_comparison_score["contig"] = (contig_bin_comparison_score["contig"] + "_" + contig_bin_comparison_score["contig_number"])
-    # contig_bin_comparison_score = contig_bin_comparison_score.drop(columns=["contig_number"])
-    
-    
+        
     return contig_bin_comparison_score
-
-
-# def process_bin_contig(bin_contig, bin_motifs_from_motifs_scored_in_bins, motifs_scored_in_contigs, choices):
-#     motif_binary_compare = pd.merge(
-#         bin_motifs_from_motifs_scored_in_bins,
-#         motifs_scored_in_contigs[motifs_scored_in_contigs["bin_compare"] == bin_contig],
-#         on="motif_mod"
-#     )
-
-#     motif_binary_compare = define_mean_methylation_thresholds(motif_binary_compare)
-
-#     if motif_binary_compare["methylation_binary_compare"].sum() == 0:
-#         return None, bin_contig
-
-#     contig_bin_comparison_score = dp.compare_methylation_pattern(motif_binary_compare, choices)
-#     return contig_bin_comparison_score, None
 
 
 def process_bin_contig(bin_contig, bin_motifs_from_motifs_scored_in_bins, motifs_scored_in_contigs, choices):
@@ -151,11 +114,6 @@ def process_bin_contig(bin_contig, bin_motifs_from_motifs_scored_in_bins, motifs
         motifs_scored_in_contigs.filter(pl.col("bin_compare") == bin_contig),
         on="motif_mod"
     )
-    # motif_binary_compare = pd.merge(
-    #     bin_motifs_from_motifs_scored_in_bins,
-    #     motifs_scored_in_contigs[motifs_scored_in_contigs["bin_compare"] == bin_contig],
-    #     on="motif_mod"
-    # )
 
     # Define methylation thresholds
     motif_binary_compare = define_mean_methylation_thresholds(motif_binary_compare)
@@ -203,69 +161,5 @@ def compare_methylation_pattern_multiprocessed(motifs_scored_in_bins, bin_consen
         if no_methylation is not None:
             contigs_w_no_methylation.append(no_methylation)
         
-    # for loop implementation
-    # for bin_contig in motifs_scored_in_contigs.select("bin_compare").unique().to_pandas()["bin_compare"].tolist():
-    #     result, no_methylation = process_bin_contig(bin_contig, bin_consensus, motifs_scored_in_contigs, choices)
-    #     if result is not None:
-    #         comparison_score = pl.concat([comparison_score, result])
-    #     if no_methylation is not None:
-    #         contigs_w_no_methylation.append(no_methylation)
-        
-    
-
-
-
     return comparison_score, contigs_w_no_methylation
 
-
-
-
-# def compare_methylation_pattern(motifs_scored_in_bins, choices, args):
-    
-#     # Step 1 create bin_motif_from_motifs_scored_in_bins - basis for bin-contig comparison
-#     bin_motifs_from_motifs_scored_in_bins = dp.construct_bin_motifs_from_motifs_scored_in_bins(
-#         motifs_scored_in_bins,
-#         args
-#     )
-    
-    
-#     ## Filter motifs that are not observed more than n_motif_cutoff times
-#     motifs_scored_in_contigs = motifs_scored_in_bins[motifs_scored_in_bins["n_motifs"] >= args.n_motif_contig_cutoff]   
-    
-#     ## Rename bin_contig to bin
-#     motifs_scored_in_contigs = motifs_scored_in_contigs[["bin_contig", "motif_mod", "mean"]]
-#     motifs_scored_in_contigs.rename(columns={"bin_contig": "bin_compare"}, inplace=True)
-    
-    
-    
-#     ## 
-#     comparison_score = pd.DataFrame()
-#     contigs_w_no_methylation = []
-    
-#     for bin_contig in motifs_scored_in_contigs["bin_compare"].unique():
-#         motif_binary_compare = pd.merge(
-#             bin_motifs_from_motifs_scored_in_bins,
-#             motifs_scored_in_contigs[motifs_scored_in_contigs["bin_compare"] == bin_contig],
-#             on = "motif_mod"
-#         )
-        
-#         motif_binary_compare = define_mean_methylation_thresholds(motif_binary_compare)
-        
-#         # If contig has no methylation, skip
-#         if motif_binary_compare["methylation_binary_compare"].sum() == 0:
-#             contigs_w_no_methylation.append(bin_contig)
-        
-        
-        
-#         contig_bin_comparison_score = dp.compare_methylation_pattern(motif_binary_compare, choices)
-        
-#         comparison_score = pd.concat([comparison_score, contig_bin_comparison_score])
-    
-        
-#     return comparison_score, contigs_w_no_methylation
-        
-        
-        
-        
-        
-    
